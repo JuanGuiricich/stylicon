7b341cd8d67592d1944415be9bb654f477e293f2
JG <131410542+JuanGuiricich@users.noreply.github.com>
2025-07-08 23:00:20 +0200

    update stylicon gem to handle background color

--- lib/stylicon/generator.rb
+++ lib/stylicon/generator.rb
@@ -19,14 +19,15 @@ module Stylicon
         width = opts["width"]
         height = opts["height"]
         classes = Array(opts["class"] || ".icon-#{name}")
+        background = opts["background"]
 
         svg = File.read(path)
         doc = Nokogiri::XML(svg)
 
         svg_node = doc.at_css("svg")
-        svg_node["fill"] = fill if fill
-        svg_node["width"] = width.to_s if width
-        svg_node["height"] = height.to_s if height
+        svg_node.remove_attribute("fill")
+        svg_node.remove_attribute("width")
+        svg_node.remove_attribute("height")
 
         doc.css("*[fill]").each { |el| el.remove_attribute("fill") }
 
@@ -34,11 +35,12 @@ module Stylicon
 
         css << <<~RULE
           #{classes.join(", ")} {
-            background-image: url("data:image/svg+xml;base64,#{encoded}");
-            background-repeat: no-repeat;
-            background-size: contain;
-            width: #{width}px;
-            height: #{height}px;
+            -webkit-mask-image: url("data:image/svg+xml;base64,#{encoded}");
+            -webkit-mask-repeat: no-repeat;
+            -webkit-mask-size: contain;
+            -webkit-mask-position: center;
+            display: inline-block;
+            #{background ? "background-color: #{background};" : ""}
           }
 
         RULE
